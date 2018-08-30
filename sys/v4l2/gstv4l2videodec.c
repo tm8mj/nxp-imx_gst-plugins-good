b2dc3ac764f4308e3f7c856c3cfe3d10fe21403f
Song Bing <bing.song@nxp.com>
2018-08-30 14:26:11 -0700

    v4l2videodec: Remove sink event task stop as flush() will do the same.
    https://bugzilla.gnome.org/show_bug.cgi?id=752962
    
    UpStream Status: Pending
    
    Signed-off-by: Song Bing<bing.song@nxp.com>
    (cherry picked from commit 2ebc731916be1fb427a0afb774f517dc67379cf5)

--- sys/v4l2/gstv4l2videodec.c
+++ sys/v4l2/gstv4l2videodec.c
@@ -1184,27 +1184,7 @@ gst_v4l2_video_dec_sink_event (GstVideoDecoder * decoder, GstEvent * event)
   gboolean ret;
   GstEventType type = GST_EVENT_TYPE (event);
 
-  switch (type) {
-    case GST_EVENT_FLUSH_START:
-      GST_DEBUG_OBJECT (self, "flush start");
-      gst_v4l2_object_unlock (self->v4l2output);
-      gst_v4l2_object_unlock (self->v4l2capture);
-      break;
-    default:
-      break;
-  }
-
   ret = GST_VIDEO_DECODER_CLASS (parent_class)->sink_event (decoder, event);
-
-  switch (type) {
-    case GST_EVENT_FLUSH_START:
-      /* The processing thread should stop now, wait for it */
-      gst_pad_stop_task (decoder->srcpad);
-      GST_DEBUG_OBJECT (self, "flush start done");
-      break;
-    default:
-      break;
-  }
 
   return ret;
 }
