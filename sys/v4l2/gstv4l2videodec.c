9d02600890a91c1c3a6ca4c73d2947d0abfca9c3
Haihua Hu <jared.hu@nxp.com>
2021-06-03 16:29:58 +0800

    v4l2videodec: always apply amphion tile modifier for amphion vpu
    imxvideoconvert_g2d and glimagesink will not propose dmabuf modifier
    meta upstream. On 8qm qxp, vpu will always output tile format,
    so no need to change modifier for amphion platform
    
    Conflicts:
    	sys/v4l2/gstv4l2videodec.c
    (cherry picked from commit 3f9b7b6d751c3e5baddd44ed2d4d7a3aeeca8e53)
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- sys/v4l2/gstv4l2videodec.c
+++ sys/v4l2/gstv4l2videodec.c
@@ -967,9 +967,12 @@ gst_v4l2_video_dec_loop (GstVideoDecoder * decoder)
 
     frame->duration = self->v4l2capture->duration;
     frame->output_buffer = buffer;
-    if ((IS_AMPHION () && self->v4l2capture->is_amphion)
-        || (IS_IMX8MQ () && self->v4l2capture->is_hantro)) {
+    if (IS_IMX8MQ () && self->v4l2capture->is_hantro) {
       guint64 drm_modifier = self->v4l2capture->drm_modifier;
+      gst_buffer_add_dmabuf_meta (frame->output_buffer, drm_modifier);
+      GST_DEBUG_OBJECT (decoder, "Add drm modifier: %lld\n", drm_modifier);
+    } else if (IS_AMPHION () && self->v4l2capture->is_amphion) {
+      guint64 drm_modifier = DRM_FORMAT_MOD_AMPHION_TILED;
       gst_buffer_add_dmabuf_meta (frame->output_buffer, drm_modifier);
       GST_DEBUG_OBJECT (decoder, "Add drm modifier: %lld\n", drm_modifier);
     }
